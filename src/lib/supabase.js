e10c878004cc379b65afa1ec880d8cbc8004f134
cherry <chrry6190@gmail.com>
2026-04-14 20:38:54 +0800

    feat: initial Vue.js Hello World setup

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseURL ='itecc6-hello'
 const supabaseKey = 'fpoygiyxdilokdknjojw'
-export const supabase = createClient(supabaseURL,supabase)
+export const supabase = createClient(supabaseURL,supabases)
